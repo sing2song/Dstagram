2ca9c41d8bde0867dde78ad65619204db2fe3bd1
songwon <sing2song@naver.com>
2018-05-01 19:56:59 +0900

    Get 메소드 실습

--- src/main/java/ac/dankook/dstagram2/controller/TemplateRestController.java
+++ src/main/java/ac/dankook/dstagram2/controller/TemplateRestController.java
@@ -4,6 +4,7 @@ import ac.dankook.dstagram2.data.Post;
 import ac.dankook.dstagram2.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +12,21 @@ public class TemplateRestController {
     //@Autowired//자동으로 인터페이스(1개일때만) 할당
     //private PostService postService;
 
-    @RequestMapping(value = "/rest/post")
-    public Post getPost() {
+    @RequestMapping(value = "/rest/post",method = RequestMethod.POST)
+    public Post createPost() {
         Post post = new Post();
 
-        post.setTitle("ssong");
+        post.setTitle("Post");
+        post.setContent("dankook univ.");
+
+        return post;
+        //return postService.getPost();//예시일 뿐 현업에서는 이렇게 사용x
+    }
+    @RequestMapping(value = "/rest/post",method = RequestMethod.GET)
+    public Post resultPost() {
+        Post post = new Post();
+
+        post.setTitle("Get");
         post.setContent("dankook univ.");
 
         return post;
--- src/main/webapp/resources/js/template.js
+++ src/main/webapp/resources/js/template.js
@@ -1,15 +1,25 @@
 'use strict'; //엄격하게 코드를 체크하는 것
 $(document).ready(function () {
   var $readMore = $('#readMore');
+  var $getMore = $('#getMore');//$는 jquery 단어라는뜻
 
   $readMore.on('click',function () {
-  $.post("/rest/post",{},function (result) {
-        console.dir(result);
-        var $content = $readMore.prev();
-        var $title = $content.prev();
+    $.post("/rest/post",{},function (result) {
+            //console.dir(result);
+           var $content = $readMore.prev();
+            var $title = $content.prev();
 
-        $title.text(result.title);
-        $content.text(result.content);
+             $title.text(result.title);
+              $content.text(result.content);
+         });
   });
-  });
+    $getMore.on('click',function () {
+        $.get("rest/post",function(result){
+            var $content = $getMore.prev();
+            var $title = $content.prev();
+
+            $title.text(result.title);
+            $content.text(result.content);
+    });
+    });
 });
